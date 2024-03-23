678340d905418c7dd6e9765e120f72ac9817da61
C0MPL3XDEV <complexdev3@gmail.com>
2024-03-23 18:51:49 +0100

    First Commit - Front-End Developing

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body style="background-color: #212529">
+<div class="container1" data-bs-theme="dark">
+    <form action="login.php" method="POST">
+        <h1>Login Form</h1>
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">@</span>
+            <input type="text" class="form-control" placeholder="Username"
+                   name="username" aria-label="Username" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-passport-fill" viewBox="0 0 16 16">
+                        <path d="M8 6a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
+                        <path d="M2 3.252a1.5 1.5 0 0 1 1.232-1.476l8-1.454A1.5 1.5 0 0 1 13 1.797v.47A2 2 0 0 1 14 4v10a2 2 0 0 1-2 2H4a2 2 0 0 1-1.51-.688 1.5 1.5 0 0 1-.49-1.11V3.253ZM5 8a3 3 0 1 0 6 0 3 3 0 0 0-6 0m0 4.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 0-1h-5a.5.5 0 0 0-.5.5"/>
+                </svg></span>
+            <input type="password" class="form-control" placeholder="Password"
+                   name="username" aria-label="Password" aria-describedby="basic-addon1">
+        </div>
+        <button type="submit" class="btn btn-primary">Sign-In</button><span> Dont have an account? <a href="register.php">Sign-Up</a> </span>
+    </form>
+</div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registration Page</title>
+    <link rel="stylesheet" href="register.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body style="background-color: #212529">
+   <div class="container" data-bs-theme="dark">
+        <form action="register.php" method="POST">
+            <h1>Registration Form</h1>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">@</span>
+                <input type="text" class="form-control" placeholder="Username"
+                       name="username" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Username" aria-label="Username">
+                <span class="input-group-text">@</span>
+                <input type="text" class="form-control" placeholder="mail.com" aria-label="Server">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-passport-fill" viewBox="0 0 16 16">
+                        <path d="M8 6a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
+                        <path d="M2 3.252a1.5 1.5 0 0 1 1.232-1.476l8-1.454A1.5 1.5 0 0 1 13 1.797v.47A2 2 0 0 1 14 4v10a2 2 0 0 1-2 2H4a2 2 0 0 1-1.51-.688 1.5 1.5 0 0 1-.49-1.11V3.253ZM5 8a3 3 0 1 0 6 0 3 3 0 0 0-6 0m0 4.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 0-1h-5a.5.5 0 0 0-.5.5"/>
+                </svg></span>
+                <input type="password" class="form-control" placeholder="Password"
+                       name="username" aria-label="Password" aria-describedby="basic-addon1">
+            </div>
+            <button type="submit" class="btn btn-primary">Sign-Up</button><span> Do you have already an account? <a href="login.php">Sign-in</a></span>
+        </form>
+   </div>
+</body>
+</html>
