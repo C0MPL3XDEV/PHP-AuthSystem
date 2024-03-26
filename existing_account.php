972689feec3c75d1b3f97d4e1ef73eb3cd767b89
C0MPL3XDEV <complexdev3@gmail.com>
2024-03-26 22:34:05 +0100

    Page for existing account's

---
+++ existing_account.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Registration Successful</title>
+    <link rel="stylesheet" href="register.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body style="background-color: #212529">
+<div class="container" data-bs-theme="dark">
+    <h1 style="align-items: center; text-align: center; position: relative; top: 80px">Sorry :( A Problem Is Occurred</h1>
+    <p style="position: relative; top: 80px">We are sorry but a user with this username or email is already in our systems please try again with other credentials or try Log-In</p>
+    <div>
+        <button class="btn btn-primary" style="position: relative; top: 80px"><a style="text-decoration: none; color: #fff" href="javascript:self.history.back()">Go Back</a></button>
+        <button class="btn btn-primary" style="position: relative; top: 80px"><a style="text-decoration: none; color: #fff" href="login.php">Log-In</a></button>
+    </div>
+</div>
+</body>
+</html>
